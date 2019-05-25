96e0f67b9d1679ffb1cbde14d5cdf53b7968f438
ferynando7 <ferynando7@gmail.com>
2019-05-25 12:39:27 -0500

    Convex hull in 3D algorithm continuation

--- library/Geometry/ConvexHull.hs
+++ library/Geometry/ConvexHull.hs
@@ -1,61 +1,96 @@
 module Geometry.ConvexHull where
 
-import Data.Graph (Graph)
 import Data.List
 import Data.Maybe
+import qualified Data.Map.Strict as MS
 import Data.Matrix hiding (trace)
 import Data.Ratio
 import Debug.Trace
+
+
 type Point3D = (Ratio Int, Ratio Int, Ratio Int)
 
-data Vertex = Vertex 
-    {
-        idVertex :: Int, -- * Identifier must be even
-        coordinates :: (Int, Int),
-        incidentEdge :: HalfEdge
-    }
+-- newtype Vertex = Vertex 
+--     {
+--         coordinates :: Point3D
+--         --incidentEdge :: HalfEdge
+--     }
 
-data Facet = Facet
-    {
-        idFacet :: Int, -- * Identifier must be odd
-        outerComponent :: HalfEdge
-    }
+-- data Facet = Facet
+--     {
+--         idFacet :: Int,
+--         outerComponent :: HalfEdge
+--     }
 
-data HalfEdge = HalfEdge
-    {
-        origin :: Vertex,
-        twin :: HalfEdge,
-        incidentFace :: Facet,
-        prev :: HalfEdge,
-        next :: HalfEdge
-    }
+-- data HalfEdge = HalfEdge
+--     {
+--         origin :: Vertex,
+--         twin :: HalfEdge,
+--        -- incidentFace :: Facet,
+--        -- prev :: HalfEdge,
+--         next :: HalfEdge
+--     }
+    
+--     | Null
 
-type ConflictGraph = Graph
+
+newtype Vertex = Vertex {coordinates :: Point3D}
+
+newtype Edge = Edge {vertices :: (Vertex, Vertex)}
+
+newtype Facet = Facet {edges :: [Edge]}
+
+newtype ConvexHull = ConvexHull {facets :: [Facet]}
+
+data ConflictGraph = ConflictGraph (MS.Map Vertex [Facet]) (MS.Map Facet [Vertex])
+
 
 
 -- | Assume every point is different
-convexHull3D :: [Point3D] -> ConflictGraph -> [Point3D]
-convexHull3D points _ =
-    | length points < 4 = points
-    | otherwise = 
+convexHull3D :: [Point3D] -> Maybe [Point3D]
+convexHull3D points
+    | length points < 4 = Just points
+    | tetraHedron == Nothing = Nothing
+    | otherwise = addPoints dcel (x:xs) conflictGraph
         where
-            tetraHedron = fromJust $ computeTetrahedron points
+            conflictGraph = startConflictGraph dcel (x:xs)
+            dcel = initializeCH . fromJust tetraHedron
+            tetraHedron = computeTetrahedron points
             (x:xs) = points // tetraHedron
 
-    let (properPoints, tetraHedron) = computeTetrahedron points
-    in convexHull3D properPoints tetraHedron (makeConflict properPoints tetraHedron)
-convexHull3D points cHull conflict
+-------------------------------------------------
 
-computeTetrahedron :: [Point3D] -> [Point3D]
-computeTetrahedron 
+startConflictGraph :: ConvexHull -> [Point3D] -> ConflictGraph
+startConflictGraph convexHull points = map (\f -> map (\p -> if isInFrontOf f p then insert f p conflictGraph else conflictGraph) points) f
+    where
+        f = facets convexHull
+        conflictGraph = MS.empty MS.empty
+        insert f p (ConflictGraph vs fs) = ConflictGraph (MS.insertWith (++) p f) (insertWith (++) f p) 
+initializeCH :: [Point3D] -> ConvexHull
+initializeCH [a,b,c,d] = ConvexHull $ map fromVertices [f1,f2,f3,f4]
+    where 
+        f1 = [a,b,c]
+        f2 = [a,c,d]
+        f3 = [a,d,b]
+        f4 = [b,d,c]
+
+
+-- | Points must be ordered counterclockwise
+fromVertices :: Point3D -> Facet
+fromVertices (a,b,c) = [Edge (va,vb), Edge (va,vc), Edge (vb,vc)]
+    where
+        [va,vb,vc] = map (Vertex) [a,b,c]
+---------------------------------------------------
+
+--
+--
+--
+--
+--
 
 
 
-
-
-
-
-
+---------------------------------------------------------------------------------------------
 computeSegment :: [Point3D] -> Maybe [Point3D]
 computeSegment [] = Nothing -- * Not enough points or all points are equal
 computeSegment (x:xs) = case find (/=x) xs of 
@@ -119,4 +154,9 @@ isCoplanar [a,b,c] d =    let
                             res = detLU matrix
                         in res == 0
 
-
+isInFrontOf :: [Point3D] -> Point3D -> Bool
+isInFrontOf [a,b,c] d =    let 
+                            matrix = fromLists (map (\(x,y,z)->[x,y,z,1]) [a,b,c,d])
+                            res = detLU matrix
+                        in res < 0
+---------------------------------------------------------------------------------------------
