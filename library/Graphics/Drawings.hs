7436f998a90ee67c3bfcca63f10ae95954da26e5
ferynando7 <ferynando7@gmail.com>
2020-01-09 10:29:55 -0500

    To Hackage

--- library/Graphics/Drawings.hs
+++ library/Graphics/Drawings.hs
@@ -17,9 +17,9 @@ fromPoint2D (x,y) = (fromIntegral x, fromIntegral y)
 tupleToList :: (Point2D, Point2D) -> [Point]
 tupleToList (p1, p2) = [fromPoint2D p1, fromPoint2D p2]
 
-makeFig :: (IsMonomialOrder ord, Ord k, Integral k)  => Polynomial k ord n -> IO ()
+makeFig :: (IsMonomialOrder ord, Ord k, Integral k)  => Polynomial k ord n  -> IO ()
 makeFig poly = let 
-                    figure = Pictures $ map (Line . tupleToList) (hypersurface poly)
+                    figure = color blue $ Pictures $ map (Line . tupleToList) (hypersurface poly)
                     scaled = scale 10.0 10.0 figure 
                 in
     display (InWindow "My Window" (400, 400) (10, 10)) white scaled
